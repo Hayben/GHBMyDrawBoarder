d05b86b33f70b5f50ff9836fac09ec82313e806c
123 <lance901221@163.com>
2016-01-08 17:31:48 +0800

    Initial Commit

---
+++ GHBDrawBoarder/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GHBDrawBoarder
+//
+//  Created by 123 on 16/1/8.
+//  Copyright © 2016年 haibin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GHBDrawBoarder/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GHBDrawBoarder
+//
+//  Created by 123 on 16/1/8.
+//  Copyright © 2016年 haibin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
